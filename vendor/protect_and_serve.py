397a509d52fb779f9c1c6502660c59db0f9ff74d
John Carr <john.carr@isotoma.com>
2015-11-22 15:07:08 +0000

    Meh

--- vendor/protect_and_serve.py
+++ vendor/protect_and_serve.py
@@ -37,6 +37,7 @@ def authorized(access_token):
 
     gh = login(token=access_token)
     for repo in gh.iter_repos():
+        print repo.full_name, app.config['REPO_NAME']
         if repo.full_name == app.config['REPO_NAME']:
             session['validated'] = True
             return redirect(next_url)
